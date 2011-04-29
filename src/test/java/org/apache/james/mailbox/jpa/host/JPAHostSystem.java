b8de9a3fe20e70a2c090dcfba6d8dfbc9e766674
Norman Maurer <norman@apache.org>
2011-04-29 07:11:40 +0000

    cascade deletes on foreignkeys
    git-svn-id: https://svn.apache.org/repos/asf/james/mailbox-integration-tester/trunk@1097708 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/james/mailbox/jpa/host/JPAHostSystem.java
+++ src/test/java/org/apache/james/mailbox/jpa/host/JPAHostSystem.java
@@ -78,7 +78,10 @@ public class JPAHostSystem extends ImapHostSystem {
 
         // Configure OpenJPA Tables creation
         properties.put("openjpa.jdbc.SynchronizeMappings", "buildSchema(ForeignKeys=true)"); // Create Foreign Keys
+        properties.put("openjpa.jdbc.MappingDefaults", "ForeignKeyDeleteAction=restrict, JoinForeignKeyDeleteAction=restrict");
         properties.put("openjpa.jdbc.SchemaFactory", "native(ForeignKeys=true)");
+        properties.put("openjpa.jdbc.MappingDefaults", "ForeignKeyDeleteAction=cascade, JoinForeignKeyDeleteAction=cascade");
+
 
         // Configure OpenJPA Cache
         properties.put("openjpa.jdbc.QuerySQLCache", "false");
