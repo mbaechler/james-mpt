e1780b5bb603431b70a47a21dc94f316f93abbe5
Benoit Tellier <btellier@apache.org>
2015-06-17 09:37:34 +0000

    MPT-15 Enable MPT tests on Cassandra backend
    git-svn-id: https://svn.apache.org/repos/asf/james/mpt/trunk@1685973 13f79535-47bb-0310-9956-ffa450edef68

--- impl/imap-mailbox/cassandra/src/test/java/org/apache/james/mpt/imapmailbox/cassandra/CassandraMailboxTest.java
+++ impl/imap-mailbox/cassandra/src/test/java/org/apache/james/mpt/imapmailbox/cassandra/CassandraMailboxTest.java
@@ -2,9 +2,7 @@ package org.apache.james.mpt.imapmailbox.cassandra;
 
 import org.apache.james.mpt.imapmailbox.AbstractMailboxTest;
 import org.apache.onami.test.annotation.GuiceModules;
-import org.junit.Ignore;
 
-@Ignore("JWC-144 Some tests are in errors")
 @GuiceModules({ CassandraMailboxTestModule.class })
 public class CassandraMailboxTest extends AbstractMailboxTest {
 
